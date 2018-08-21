03425177ec3b66373993924d529c6fc325a034ee
Insayder <dm7rin@gmail.com>
2018-08-21 22:34:13 +0300

    add js

---
+++ code.js
@@ -0,0 +1,29 @@
+document.querySelectorAll(".checkbox").forEach(function(checkbox){
+    checkbox.addEventListener("click",function(){document.querySelector(".flood").classList.add("color_flood")});
+
+   /*  checkbox.addEventListener("click", function(){
+        document.querySelectorAll(".block").forEach(function(block){
+            block.classList.add(".color_flood"); 
+        })
+    })*/
+});
+
+
+
+
+
+
+
+
+/* .forEach(function(checkBox){ 
+    console.log()
+    }) */
+
+
+
+
+
+
+
+   /*  if(checkBox.checked){checkBox.querySelector(".block").classList.add("color_flood")} */
+/* block.addEventListener("click", function(){block.querySelector("img").classList.add("color_flood") */
