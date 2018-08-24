3c5cc53ec262e1e4dea4c4a79e2c80e5868e9ab0
Insayder <dm7rin@gmail.com>
2018-08-24 20:44:44 +0300

    ругается гит

--- code.js
+++ code.js
@@ -1,16 +1,4 @@
-/* document.querySelectorAll(".block").forEach(function(block){    
-    block.addEventListener("change", function(){        
-        document.querySelectorAll(".checkbox").forEach(function(checkbox){
-            addEventListener("change", function(){
-                block.classList.add("color_flood");
-            })
-        })
-    })
-})
-    */    
-      
    document.querySelectorAll(".block").forEach(function(block){ 
-
     block.querySelector(".checkbox").addEventListener("change", function(){ 
     block.classList.add("color_flood");     
     }) 
